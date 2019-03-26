45ce76dd8f953aa09f3dc2db9b1804d4ccb5033b
Ivraj Seerha <iseerha@college.harvard.edu>
2019-03-26 13:58:55 -0700

    Removes schema author from about

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,40 +7,60 @@ import './about.sass'
 const About = ({
   data: {
     blogData: { name, description },
-    allAuthor: { edges: authors },
+    allAuthor,
   },
-}) => (
-  <Layout>
-    <div className="about-container">
-      <h1 id="authorheader">About {name}</h1>
-      <div className="body-text">{description}</div>
-      <div style={{ height: '50px' }} />
-      <div className="writers">
-        <h2>Writers</h2>
-        {authors.map(({ node }) => (
-          <Link key={node.id} className="writers__entry" to={node.slug}>
-            <Img
-              alt={`${node.name} headshot`}
-              fixed={node.headshot.childImageSharp.fixed}
-              className="writers__headshot"
-            />
-            <div style={{ width: '30px' }} />
-            <div className="writers__text">
-              <span className="writers__name">{node.name}</span>
-              <p className="writers__bio">{node.bio}</p>
-            </div>
-          </Link>
-        ))}
+}) => {
+  let AboutContents = (
+    <Layout>
+      <div className="about-container">
+        <h1 id="authorheader">About {name}</h1>
+        <div className="body-text">{description}</div>
+        <div style={{ height: '50px' }} />
       </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+
+  if (allAuthor !== null) {
+    const { edges: authors } = allAuthor
+    AboutContents = (
+      <Layout>
+        <div className="about-container">
+          <h1 id="authorheader">About {name}</h1>
+          <div className="body-text">{description}</div>
+          <div style={{ height: '50px' }} />
+          <div className="writers">
+            <h2>Writers</h2>
+            {authors.map(({ node }) => (
+              <Link key={node.id} className="writers__entry" to={node.slug}>
+                <Img
+                  alt={`${node.name} headshot`}
+                  fixed={node.headshot.childImageSharp.fixed}
+                  className="writers__headshot"
+                />
+                <div style={{ width: '30px' }} />
+                <div className="writers__text">
+                  <span className="writers__name">{node.name}</span>
+                  <p className="writers__bio">{node.bio}</p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+  return AboutContents
+}
 
 export default About
 
 export const pageQuery = graphql`
   query AboutQuery {
-    allAuthor {
+    allAuthor(
+      filter: {
+        id: { ne: "blogwise-author-fcae3044-6a1e-4c20-909a-aa41d09bc001" }
+      }
+    ) {
       edges {
         node {
           id
