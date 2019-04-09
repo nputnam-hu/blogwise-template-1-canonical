c5ed1bcc7cb20fac378cd386125ec67bf52c26c6
Ivraj Seerha <iseerha@college.harvard.edu>
2019-04-08 23:11:49 -0700

    Fixes footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { StaticQuery, graphql, Link } from 'gatsby'
-import './styles.sass'
+import Img from 'gatsby-image'
+
+import styles from './Footer.module.sass'
 
 const Footer = () => (
   <StaticQuery
@@ -9,26 +11,45 @@ const Footer = () => (
         blogData {
           name
           mainSiteUrl
+          sidebar {
+            childImageSharp {
+              fixed(height: 35) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
         }
       }
     `}
-    render={({ blogData: { name, mainSiteUrl } }) => (
-      <div id="footer-container">
-        <div id="footerlinks">
-          <Link to="/about">ABOUT</Link>
-          <Link to="/search">SEARCH</Link>
-          <a href={mainSiteUrl} target="_blank" rel="noopener noreferrer">
-            Visit {name}
-          </a>
-        </div>
-        <div id="poweredby">
-          <a
-            href="https://www.blogwise.co?rel=blog"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            powered by blogwise
-          </a>
+    render={({ blogData: { name, mainSiteUrl, sidebar } }) => (
+      <div className={styles.Footer}>
+        <div className={styles.Footer__container}>
+          <div className={styles.Footer__links}>
+            <Link to="/about" className={styles.Footer__links__link}>
+              ABOUT
+            </Link>
+            <Link to="/search" className={styles.Footer__links__link}>
+              SEARCH
+            </Link>
+            <a
+              href={mainSiteUrl}
+              className={styles.Footer__links__link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Visit {name}
+            </a>
+          </div>
+          <div className={styles.Footer__poweredBy}>
+            <a
+              href="https://www.blogwise.co?rel=blog"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              powered by blogwise
+            </a>
+            <Img alt={`${name} logo`} fixed={sidebar.childImageSharp.fixed} />
+          </div>
         </div>
       </div>
     )}
