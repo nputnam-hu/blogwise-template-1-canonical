8e173b427500dc2ceaa1d69e7adafea0cf883917
Ivraj Seerha <iseerha@college.harvard.edu>
2019-04-24 12:31:34 -0700

    Fixes header bugs DONT MERGE hasBeenInit is true

--- downloadConfig.js
+++ downloadConfig.js
@@ -6,7 +6,7 @@ const defaultConfig = {
   token:
     'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImY3ZTExYzZhLWM2N2EtNDI2ZS04ZTM1LWQ1OTM5YzhlMzUwZSIsImVtYWlsIjoic2FtK2JhcmFja0BydWJpbmthcGxhbi5jb20ifQ.R4cHtEvrgLiPw3SpNzg0jbvThWB9bi_8471oaTxyRZ8',
   apiUrl: 'https://megaphone-api-prod.herokuapp.com',
-  hasBeenInitialized: false,
+  hasBeenInitialized: true,
 }
 
 fs.writeFileSync(
