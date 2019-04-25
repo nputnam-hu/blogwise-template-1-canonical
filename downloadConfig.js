172eccb09894980a92aaefdfa70b44ebf7cb10df
Ivraj Seerha <iseerha@college.harvard.edu>
2019-04-25 12:31:24 -0400

    Testing preview env var

--- downloadConfig.js
+++ downloadConfig.js
@@ -3,8 +3,7 @@ const fs = require('fs')
 const defaultConfig = {
   faviconPhotoUri: '',
   customNavbarLinks: [],
-  token:
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImIwZWQzMTU4LTU3YTAtNGQxMS05Mjk5LWM2NTUwMDNhMzI4ZSIsImVtYWlsIjoiZGFyYUBibG9nd2lzZS5jbyJ9.J4sjfTqHcNH13jF2DXayzB1D3JobHKlK3zGfWalPnTA',
+  token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImIwZWQzMTU4LTU3YTAtNGQxMS05Mjk5LWM2NTUwMDNhMzI4ZSIsImVtYWlsIjoiZGFyYUBibG9nd2lzZS5jbyJ9.J4sjfTqHcNH13jF2DXayzB1D3JobHKlK3zGfWalPnTA',
   apiUrl: 'https://megaphone-api-prod.herokuapp.com',
   hasBeenInitialized: false,
 }
@@ -13,3 +12,7 @@ fs.writeFileSync(
   './config.json',
   process.env.INCOMING_HOOK_BODY || JSON.stringify(defaultConfig),
 )
+
+if (process.env.PREVIEW) {
+  console.log('Rendering preview 1')
+}
